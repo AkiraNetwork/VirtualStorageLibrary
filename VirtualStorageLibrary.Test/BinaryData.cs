4823d1665af3be0e75f1d58741e4bfe031b07962
下舘朗 <shimodateakira@i.softbank.jp>
2024-01-13 20:39:46 +0900

    テストプロジェクトの追加。

---
+++ VirtualStorageLibrary.Test/BinaryData.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace VirtualStorageLibrary.Test
+{
+    public class BinaryData
+        : IDeepCloneable<BinaryData>, IEnumerable<byte>, IList<byte>, ICollection<byte>, IEquatable<BinaryData>
+    {
+        private byte[] _data;
+        private ReadOnlyCollection<byte> _readOnlyDataCache;
+
+        public BinaryData()
+        {
+            _data = [];
+            _readOnlyDataCache = new(_data);
+        }
+
+        public BinaryData(IEnumerable<byte> data)
+        {
+            if(data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "dataはnullにできません。");
+            }
+            _data = data.ToArray();
+            _readOnlyDataCache = new(_data);
+        }
+
+        public ReadOnlyCollection<byte> Data
+        {
+            get
+            {
+                if (_readOnlyDataCache == null)
+                {
+                    _readOnlyDataCache = new(_data);
+                }
+                return _readOnlyDataCache;
+            }
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _data.Length)
+                {
+                    throw new IndexOutOfRangeException("インデックスが配列の範囲外です。");
+                }
+                return _data[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _data.Length)
+                {
+                    throw new IndexOutOfRangeException("インデックスが配列の範囲外です。");
+                }
+                _data[index] = value;
+            }
+        }
+
+        public int Count => _data.Length;
+
+        public bool IsReadOnly => false;
+
+        public void Add(byte item)
+        {
+            byte[] newData = new byte[_data.Length + 1];
+            _data.CopyTo(newData, 0);
+            newData[_data.Length] = item;
+            _data = newData;
+        }
+
+        public void Clear()
+        {
+            _data = [];
+            _readOnlyDataCache = new(_data);
+        }
+
+        public bool Contains(byte item) => _data.Contains(item);
+
+        public void CopyTo(byte[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0 || arrayIndex > array.Length)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), "インデックスが配列の範囲外です。");
+            if (array.Length - arrayIndex < _data.Length)
+                throw new ArgumentException("配列の残りの長さが不足しています。");
+
+            _data.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<byte> GetEnumerator() => ((IEnumerable<byte>)_data).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public int IndexOf(byte item)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (_data[i] == item)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, byte item)
+        {
+            if (index < 0 || index > _data.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "indexは配列の範囲外です。");
+            }
+
+            byte[] newData = new byte[_data.Length + 1];
+            Array.Copy(_data, 0, newData, 0, index);
+            newData[index] = item;
+            Array.Copy(_data, index, newData, index + 1, _data.Length - index);
+            _data = newData;
+        }
+
+        public bool Remove(byte item)
+        {
+            int index = IndexOf(item);
+            if (index < 0) return false;
+            RemoveAt(index);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _data.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "indexは配列の範囲外です。");
+            }
+
+            byte[] newData = new byte[_data.Length - 1];
+            Array.Copy(_data, 0, newData, 0, index);
+            Array.Copy(_data, index + 1, newData, index, _data.Length - index - 1);
+            _data = newData;
+        }
+
+        public BinaryData DeepClone()
+        {
+            byte[] dataClone = new byte[_data.Length];
+            _data.CopyTo(dataClone, 0);
+            return new BinaryData(dataClone);
+        }
+
+        public bool Equals(BinaryData? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return _data.SequenceEqual(other._data);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is BinaryData other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+            foreach (byte b in _data)
+            {
+                hash.Add(b);
+            }
+            return hash.ToHashCode();
+        }
+
+        public static bool operator ==(BinaryData? left, BinaryData? right)
+        {
+            return left is null ? right is null : left.Equals(right);
+        }
+
+        public static bool operator !=(BinaryData? left, BinaryData? right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ VirtualStorageLibrary.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace VirtualStorageLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
