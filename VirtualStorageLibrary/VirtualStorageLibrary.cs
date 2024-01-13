3697de6163ce64455732416709c854ccdac7a2a4
下舘朗 <shimodateakira@i.softbank.jp>
2024-01-13 20:08:05 +0900

    プロジェクト ファイルを追加します。

---
+++ VirtualStorageLibrary/VirtualStorageLibrary.cs
@@ -0,0 +1,78 @@
+﻿namespace VirtualStorageLibrary
+{
+    public interface IDeepCloneable<T>
+    {
+        T DeepClone();
+    }
+
+    public abstract class VirtualNode : IDeepCloneable<VirtualNode>
+    {
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public abstract VirtualNode DeepClone();
+        protected VirtualNode(string name)
+        {
+            Name = name;
+            CreateDate = DateTime.Now;
+            UpdateDate = DateTime.Now;
+        }
+    }
+
+    public class VirtualItem<T> : VirtualNode, IDeepCloneable<VirtualItem<T>> where T : IDeepCloneable<T>
+    {
+        public T Item { get; set; }
+
+        public VirtualItem(string name, T item) : base(name)
+        {
+            Item = item;
+        }
+
+        public override VirtualNode DeepClone()
+        {
+            return new VirtualItem<T>(Name, Item.DeepClone());
+        }
+
+        VirtualItem<T> IDeepCloneable<VirtualItem<T>>.DeepClone()
+        {
+            return (VirtualItem<T>)DeepClone();
+        }
+    }
+
+    public class VirtualDirectory : VirtualNode, IDeepCloneable<VirtualDirectory>
+    {
+        public Dictionary<string, VirtualNode> Nodes { get; set; }
+
+        public VirtualDirectory(string name) : base(name)
+        {
+            Nodes = new Dictionary<string, VirtualNode>();
+        }
+
+        public override VirtualNode DeepClone()
+        {
+            var clonedDirectory = new VirtualDirectory(this.Name)
+            {
+                CreateDate = this.CreateDate,
+                UpdateDate = this.UpdateDate,
+                Nodes = new Dictionary<string, VirtualNode>()
+            };
+
+            foreach (var pair in this.Nodes)
+            {
+                clonedDirectory.Nodes.Add(pair.Key, pair.Value.DeepClone());
+            }
+
+            return clonedDirectory;
+        }
+
+        VirtualDirectory IDeepCloneable<VirtualDirectory>.DeepClone()
+        {
+            return (VirtualDirectory)DeepClone();
+        }
+    }
+
+    public class VirtualStorage
+    {
+        public VirtualDirectory Root { get; set; }
+    }
+}
