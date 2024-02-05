f5893309c1ebff738e7955cb2c8d7fcc94ccc187
下舘朗 <shimodateakira@i.softbank.jp>
2024-02-05 21:37:42 +0900

    BinaryDataクラスのLengthプロパティをCountプロパティに戻し、新たにLengthプロパティを追加した。

--- VirtualStorageLibrary.Test/BinaryData.cs
+++ VirtualStorageLibrary.Test/BinaryData.cs
@@ -57,6 +57,8 @@ namespace VirtualStorageLibrary.Test
             }
         }
 
+        public int Count => _data.Length;
+
         public int Length => _data.Length;
 
         public bool IsReadOnly => false;
