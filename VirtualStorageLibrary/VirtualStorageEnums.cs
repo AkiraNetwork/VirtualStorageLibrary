75e595cd4b4f4515a3f24b4ce7f8c76e50cd080e
下舘朗 <shimodateakira@i.softbank.jp>
2024-05-26 12:43:25 +0900

    不要なusingの削除。

--- VirtualStorageLibrary/VirtualStorageEnums.cs
+++ VirtualStorageLibrary/VirtualStorageEnums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AkiraNet.VirtualStorageLibrary
+﻿namespace AkiraNet.VirtualStorageLibrary
 {
     public enum VirtualNodeType
     {
