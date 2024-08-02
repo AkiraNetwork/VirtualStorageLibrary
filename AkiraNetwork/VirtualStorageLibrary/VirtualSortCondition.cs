e2ffb9eddead36cffcfb3d68191f9606121f090b
下舘朗 <shimodateakira@i.softbank.jp>
2024-08-02 18:35:19 +0900

    #138 VirtualSortConditionについて説明を追加した。テスト済み。

--- AkiraNetwork/VirtualStorageLibrary/VirtualSortCondition.cs
+++ AkiraNetwork/VirtualStorageLibrary/VirtualSortCondition.cs
@@ -1,9 +1,37 @@
 ﻿namespace AkiraNetwork.VirtualStorageLibrary
 {
-    public class VirtualSortCondition<T>(Expression<Func<T, object>> sortBy, bool ascending = true)
+    /// <summary>
+    /// Represents the conditions for sorting data, holding the property to sort by and the order (ascending or descending).
+    /// </summary>
+    /// <typeparam name="T">The type of the entity to be sorted.</typeparam>
+    public class VirtualSortCondition<T>
     {
-        public Expression<Func<T, object>> SortBy { get; set; } = sortBy;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualSortCondition{T}"/> class with the specified sorting property and order.
+        /// </summary>
+        /// <param name="sortBy">The property to sort by.</param>
+        /// <param name="ascending">A value indicating whether the sorting order is ascending.</param>
+        public VirtualSortCondition(Expression<Func<T, object>> sortBy, bool ascending = true)
+        {
+            SortBy = sortBy;
+            Ascending = ascending;
+        }
 
-        public bool Ascending { get; set; } = ascending;
+        /// <summary>
+        /// Gets or sets the property used for sorting.
+        /// </summary>
+        /// <value>
+        /// An expression that specifies the property to use for sorting.
+        /// </value>
+        public Expression<Func<T, object>> SortBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the sorting order is ascending.
+        /// True if the order is ascending; otherwise, false.
+        /// </summary>
+        /// <value>
+        /// A boolean value indicating whether the sorting order is ascending.
+        /// </value>
+        public bool Ascending { get; set; }
     }
 }
