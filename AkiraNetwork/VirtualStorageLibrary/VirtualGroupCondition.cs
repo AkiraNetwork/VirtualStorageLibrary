407797600f7d0faef813fd37bf4b260b3eecbc31
下舘朗 <shimodateakira@i.softbank.jp>
2024-08-02 18:29:19 +0900

    #138 VirtualGroupConditionでプライマリーコンストラクターの記述形式を解除し、説明を付けなおした。ドキュメントコメントは対応してないらしい。また、IDE0290の警告を非表示にするように.editorconfigを追加した。テスト済み。

--- AkiraNetwork/VirtualStorageLibrary/VirtualGroupCondition.cs
+++ AkiraNetwork/VirtualStorageLibrary/VirtualGroupCondition.cs
@@ -5,17 +5,34 @@
     /// </summary>
     /// <typeparam name="T">The type of the entity to be grouped.</typeparam>
     /// <typeparam name="TKey">The type of the key used for grouping.</typeparam>
-    public class VirtualGroupCondition<T, TKey>(Expression<Func<T, TKey>> groupBy, bool ascending = true)
+    public class VirtualGroupCondition<T, TKey>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualGroupCondition{T, TKey}"/> class with the specified grouping property and order.
+        /// </summary>
+        /// <param name="groupBy">The property to group by.</param>
+        /// <param name="ascending">A value indicating whether the grouping order is ascending.</param>
+        public VirtualGroupCondition(Expression<Func<T, TKey>> groupBy, bool ascending = true)
+        {
+            GroupBy = groupBy;
+            Ascending = ascending;
+        }
+
         /// <summary>
         /// Gets or sets the property used for grouping.
         /// </summary>
-        public Expression<Func<T, TKey>> GroupBy { get; set; } = groupBy;
+        /// <value>
+        /// An expression that specifies the property to use for grouping.
+        /// </value>
+        public Expression<Func<T, TKey>> GroupBy { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the grouping order is ascending.
-        /// True for ascending, false for descending.
+        /// True if the order is ascending; otherwise, false.
         /// </summary>
-        public bool Ascending { get; set; } = ascending;
+        /// <value>
+        /// A boolean value indicating whether the grouping order is ascending.
+        /// </value>
+        public bool Ascending { get; set; }
     }
 }
