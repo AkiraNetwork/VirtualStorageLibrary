727297955dada7f9192b07176d1b4af764ec5df8
下舘朗 <shimodateakira@i.softbank.jp>
2024-08-03 20:30:16 +0900

    #138 アダプタークラスについて説明を追加。テスト済み。

--- AkiraNetwork/VirtualStorageLibrary/VirtualAdapter.cs
+++ AkiraNetwork/VirtualStorageLibrary/VirtualAdapter.cs
@@ -2,10 +2,34 @@
 
 namespace AkiraNetwork.VirtualStorageLibrary
 {
-    public class VirtualItemAdapter<T>(VirtualStorage<T> storage)
+    /// <summary>
+    /// Adapter class to simplify operations on virtual items.
+    /// Reduces the need for casting and simplifies code, allowing
+    /// users to easily manage specific node types.
+    /// </summary>
+    /// <typeparam name="T">The type of item stored in the virtual storage.</typeparam>
+    public class VirtualItemAdapter<T>
     {
-        private readonly VirtualStorage<T> _storage = storage;
+        private readonly VirtualStorage<T> _storage;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualItemAdapter{T}"/> class.
+        /// </summary>
+        /// <param name="storage">The virtual storage instance.</param>
+        public VirtualItemAdapter(VirtualStorage<T> storage)
+        {
+            _storage = storage;
+        }
+
+        /// <summary>
+        /// Gets or sets the virtual item corresponding to the specified path.
+        /// </summary>
+        /// <param name="path">The path of the item.</param>
+        /// <param name="followLinks">A flag indicating whether to follow links.</param>
+        /// <value>The <see cref="VirtualItem{T}"/> corresponding to the specified path.</value>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the node is not a <see cref="VirtualItem{T}"/>.
+        /// </exception>
         public VirtualItem<T> this[VirtualPath path, bool followLinks = true]
         {
             get
@@ -24,10 +48,34 @@ namespace AkiraNetwork.VirtualStorageLibrary
         }
     }
 
-    public class VirtualDirectoryAdapter<T>(VirtualStorage<T> storage)
+    /// <summary>
+    /// Adapter class to simplify operations on virtual directories.
+    /// Reduces the need for casting and simplifies code, allowing
+    /// users to easily manage specific node types.
+    /// </summary>
+    /// <typeparam name="T">The type of item stored in the virtual storage.</typeparam>
+    public class VirtualDirectoryAdapter<T>
     {
-        private readonly VirtualStorage<T> _storage = storage;
+        private readonly VirtualStorage<T> _storage;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualDirectoryAdapter{T}"/> class.
+        /// </summary>
+        /// <param name="storage">The virtual storage instance.</param>
+        public VirtualDirectoryAdapter(VirtualStorage<T> storage)
+        {
+            _storage = storage;
+        }
+
+        /// <summary>
+        /// Gets or sets the virtual directory corresponding to the specified path.
+        /// </summary>
+        /// <param name="path">The path of the directory.</param>
+        /// <param name="followLinks">A flag indicating whether to follow links.</param>
+        /// <value>The <see cref="VirtualDirectory"/> corresponding to the specified path.</value>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the node is not a <see cref="VirtualDirectory"/>.
+        /// </exception>
         public VirtualDirectory this[VirtualPath path, bool followLinks = true]
         {
             get
@@ -46,10 +94,38 @@ namespace AkiraNetwork.VirtualStorageLibrary
         }
     }
 
-    public class VirtualSymbolicLinkAdapter<T>(VirtualStorage<T> storage)
+    /// <summary>
+    /// Adapter class to simplify operations on virtual symbolic links.
+    /// Reduces the need for casting and simplifies code, allowing
+    /// users to easily manage specific node types.
+    /// </summary>
+    /// <typeparam name="T">The type of item stored in the virtual storage.</typeparam>
+    public class VirtualSymbolicLinkAdapter<T>
     {
-        private readonly VirtualStorage<T> _storage = storage;
+        private readonly VirtualStorage<T> _storage;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirtualSymbolicLinkAdapter{T}"/> class.
+        /// </summary>
+        /// <param name="storage">The virtual storage instance.</param>
+        public VirtualSymbolicLinkAdapter(VirtualStorage<T> storage)
+        {
+            _storage = storage;
+        }
+
+        /// <summary>
+        /// Gets or sets the virtual symbolic link corresponding to the specified path.
+        /// </summary>
+        /// <param name="path">The path of the symbolic link.</param>
+        /// <param name="followLinks">A flag indicating whether to follow links.</param>
+        /// <value>The <see cref="VirtualSymbolicLink"/> corresponding to the specified path.</value>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the node is not a <see cref="VirtualSymbolicLink"/>.
+        /// </exception>
+        /// <remarks>
+        /// To operate on the symbolic link itself, specify `followLinks` as false.
+        /// Note that the default value of this parameter may change in the future.
+        /// </remarks>
         public VirtualSymbolicLink this[VirtualPath path, bool followLinks = true]
         {
             get
