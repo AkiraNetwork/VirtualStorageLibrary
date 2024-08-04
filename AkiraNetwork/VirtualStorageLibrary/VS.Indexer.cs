8b488515dfdadbde4ec3ecda4690558ba2807894
下舘朗 <shimodateakira@i.softbank.jp>
2024-08-04 14:55:13 +0900

    #138 VirtualStorage、VS.Indexerの説明を追加した。テスト済み。

--- AkiraNetwork/VirtualStorageLibrary/VS.Indexer.cs
+++ AkiraNetwork/VirtualStorageLibrary/VS.Indexer.cs
@@ -4,13 +4,30 @@ namespace AkiraNetwork.VirtualStorageLibrary
 {
     public partial class VirtualStorage<T>
     {
+        /// <summary>
+        /// Gets the adapter providing operations for virtual items.
+        /// </summary>
+        /// <value>An instance of <see cref="VirtualItemAdapter{T}"/></value>
         public VirtualItemAdapter<T> Item { get; }
 
+        /// <summary>
+        /// Gets the adapter providing operations for virtual directories.
+        /// </summary>
+        /// <value>An instance of <see cref="VirtualDirectoryAdapter{T}"/></value>
         public VirtualDirectoryAdapter<T> Dir { get; }
 
+        /// <summary>
+        /// Gets the adapter providing operations for virtual symbolic links.
+        /// </summary>
+        /// <value>An instance of <see cref="VirtualSymbolicLinkAdapter{T}"/></value>
         public VirtualSymbolicLinkAdapter<T> Link { get; }
 
-        // パスを受け取るインデクサ
+        /// <summary>
+        /// Gets or sets the virtual node corresponding to the specified virtual path.
+        /// </summary>
+        /// <param name="path">The virtual path</param>
+        /// <param name="followLinks">Flag indicating whether to follow symbolic links</param>
+        /// <value>The virtual node corresponding to the specified virtual path</value>
         [IndexerName("Indexer")]
         public VirtualNode this[VirtualPath path, bool followLinks = true]
         {
--- AkiraNetwork/VirtualStorageLibrary/VirtualStorage.cs
+++ AkiraNetwork/VirtualStorageLibrary/VirtualStorage.cs
@@ -1,22 +1,40 @@
-﻿using AkiraNetwork.VirtualStorageLibrary.Localization;
-using System.Runtime.CompilerServices;
-
-namespace AkiraNetwork.VirtualStorageLibrary
+﻿namespace AkiraNetwork.VirtualStorageLibrary
 {
+    /// <summary>
+    /// Manages virtual storage containing user-defined type T item data.
+    /// </summary>
+    /// <typeparam name="T">The user-defined type within the virtual storage</typeparam>
     public partial class VirtualStorage<T>
     {
         private readonly VirtualDirectory _root;
 
+        /// <summary>
+        /// Gets the root directory of the virtual storage.
+        /// </summary>
+        /// <value>The root directory of the virtual storage</value>
         public VirtualDirectory Root => _root;
 
+        /// <summary>
+        /// Gets or sets the current virtual path.
+        /// </summary>
+        /// <value>The current virtual path</value>
         public VirtualPath CurrentPath { get; private set; }
 
-        // 循環参照検出クラス(WalkPathToTargetメソッド用)
+        /// <summary>
+        /// Gets the cycle detector class used for the WalkPathToTarget method.
+        /// </summary>
+        /// <value>An instance of the cycle detector class</value>
         public VirtualCycleDetector CycleDetectorForTarget { get; } = new();
 
-        // 循環参照検出クラス(WalkPathTreeメソッド用)
+        /// <summary>
+        /// Gets the cycle detector class used for the WalkPathTree method.
+        /// </summary>
+        /// <value>An instance of the cycle detector class</value>
         public VirtualCycleDetector CycleDetectorForTree { get; } = new();
 
+        /// <summary>
+        /// Initializes a new instance of the VirtualStorage class.
+        /// </summary>
         public VirtualStorage()
         {
             _root = new(VirtualPath.Root)
