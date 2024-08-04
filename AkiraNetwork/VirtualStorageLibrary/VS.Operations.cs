1c1ef537911dbbfda76010e11038295b08c6ec76
下舘朗 <shimodateakira@i.softbank.jp>
2024-08-04 16:43:28 +0900

    #138 VS.Operationsに説明を追加した。テスト済み。

--- AkiraNetwork/VirtualStorageLibrary/VS.Operations.cs
+++ AkiraNetwork/VirtualStorageLibrary/VS.Operations.cs
@@ -4,6 +4,10 @@ namespace AkiraNetwork.VirtualStorageLibrary
 {
     public partial class VirtualStorage<T>
     {
+        /// <summary>
+        /// Changes the current working directory.
+        /// </summary>
+        /// <param name="path">The path of the new working directory</param>
         public void ChangeDirectory(VirtualPath path)
         {
             path = ConvertToAbsolutePath(path).NormalizePath();
@@ -13,6 +17,11 @@ namespace AkiraNetwork.VirtualStorageLibrary
             return;
         }
 
+        /// <summary>
+        /// Sets a node at the specified path.
+        /// </summary>
+        /// <param name="destinationPath">The path where the node is set</param>
+        /// <param name="node">The <see cref="VirtualNode"/> to set</param>
         public void SetNode(VirtualPath destinationPath, VirtualNode node)
         {
             destinationPath = ConvertToAbsolutePath(destinationPath).NormalizePath();
@@ -33,43 +42,64 @@ namespace AkiraNetwork.VirtualStorageLibrary
             }
         }
 
+        /// <summary>
+        /// Updates an item.
+        /// </summary>
+        /// <param name="itemPath">The path of the item to update</param>
+        /// <param name="newItem">The new data for the item</param>
         public void UpdateItem(VirtualPath itemPath, VirtualItem<T> newItem)
         {
-            // 絶対パスに変換
+            // Convert to absolute path
             itemPath = ConvertToAbsolutePath(itemPath).NormalizePath();
 
-            // 既存のアイテムを取得
+            // Retrieve the existing item
             VirtualItem<T> item = GetItem(itemPath, true);
 
-            // 既存アイテムのデータを更新
+            // Update the data of the existing item
             item.Update(newItem);
         }
 
+        /// <summary>
+        /// Updates a directory.
+        /// </summary>
+        /// <param name="directoryPath">The path of the directory to update</param>
+        /// <param name="newDirectory">The new data for the directory</param>
         public void UpdateDirectory(VirtualPath directoryPath, VirtualDirectory newDirectory)
         {
-            // 絶対パスに変換
+            // Convert to absolute path
             directoryPath = ConvertToAbsolutePath(directoryPath).NormalizePath();
 
-            // 既存のディレクトリを取得
+            // Retrieve the existing directory
             VirtualDirectory directory = GetDirectory(directoryPath, true);
 
-            // 既存ディレクトリのノードを更新
+            // Update the nodes of the existing directory
             directory.Update(newDirectory);
         }
 
+        /// <summary>
+        /// Updates a symbolic link.
+        /// </summary>
+        /// <param name="linkPath">The path of the symbolic link to update</param>
+        /// <param name="newLink">The new data for the symbolic link</param>
         public void UpdateSymbolicLInk(VirtualPath linkPath, VirtualSymbolicLink newLink)
         {
-            // 絶対パスに変換
+            // Convert to absolute path
             linkPath = ConvertToAbsolutePath(linkPath).NormalizePath();
 
-            // 既存のシンボリックリンクを取得
-            // シンボリックリンクの場合、パス解決をするのは無意味なので followLinks は false固定
+            // Retrieve the existing symbolic link
+            // For symbolic links, resolving the path is meaningless, so followLinks is set to false
             VirtualSymbolicLink link = GetSymbolicLink(linkPath);
 
-            // 既存シンボリックリンクのノードを更新
+            // Update the nodes of the existing symbolic link
             link.Update(newLink);
         }
 
+        /// <summary>
+        /// Adds a node to the specified directory.
+        /// </summary>
+        /// <param name="nodeDirectoryPath">The path of the directory to add the node to</param>
+        /// <param name="node">The <see cref="VirtualNode"/> to add</param>
+        /// <param name="overwrite">Indicates whether to overwrite an existing node</param>
         public void AddNode(VirtualPath nodeDirectoryPath, VirtualNode node, bool overwrite = false)
         {
             nodeDirectoryPath = ConvertToAbsolutePath(nodeDirectoryPath).NormalizePath();
@@ -90,6 +120,12 @@ namespace AkiraNetwork.VirtualStorageLibrary
             }
         }
 
+        /// <summary>
+        /// Adds a directory.
+        /// </summary>
+        /// <param name="directoryPath">The path of the directory to add</param>
+        /// <param name="directory">The <see cref="VirtualDirectory"/> to add</param>
+        /// <param name="createSubdirectories">Indicates whether to create intermediate directories</param>
         public void AddDirectory(VirtualPath directoryPath, VirtualDirectory directory, bool createSubdirectories = false)
         {
             directoryPath = ConvertToAbsolutePath(directoryPath).NormalizePath();
@@ -112,10 +148,10 @@ namespace AkiraNetwork.VirtualStorageLibrary
                     throw new InvalidOperationException(string.Format(Resources.NodeAlreadyExists, directory.Name));
                 }
 
-                // 新しいディレクトリを追加
+                // Add the new directory
                 directory = (VirtualDirectory)parentDirectory.Add(directory);
 
-                // 全てのターゲットノードタイプを更新
+                // Update all target node types in the dictionary
                 UpdateAllTargetNodeTypesInDictionary();
             }
             else
@@ -126,6 +162,11 @@ namespace AkiraNetwork.VirtualStorageLibrary
             return;
         }
 
+        /// <summary>
+        /// Adds a directory.
+        /// </summary>
+        /// <param name="path">The path of the directory to add</param>
+        /// <param name="createSubdirectories">Indicates whether to create intermediate directories</param>
         public void AddDirectory(VirtualPath path, bool createSubdirectories = false)
         {
             path = ConvertToAbsolutePath(path).NormalizePath();
@@ -142,28 +183,41 @@ namespace AkiraNetwork.VirtualStorageLibrary
             AddDirectory(directoryPath, newDirectory, createSubdirectories);
         }
 
+        /// <summary>
+        /// Creates intermediate directories.
+        /// </summary>
+        /// <param name="parentDirectory">The parent directory</param>
+        /// <param name="nodeName">The name of the new node</param>
+        /// <param name="nodePath">The path of the new node</param>
+        /// <returns>True if the intermediate directory was successfully created</returns>
         private bool CreateIntermediateDirectory(VirtualDirectory parentDirectory, VirtualNodeName nodeName, VirtualPath nodePath)
         {
             VirtualDirectory newSubdirectory = new(nodeName);
 
-            // 中間ディレクトリを追加
+            // Add the intermediate directory
             parentDirectory.Add(newSubdirectory);
 
-            // 全てのターゲットノードタイプを更新
+            // Update all target node types in the dictionary
             UpdateAllTargetNodeTypesInDictionary();
 
             return true;
         }
 
+        /// <summary>
+        /// Adds an item.
+        /// </summary>
+        /// <param name="itemDirectoryPath">The path of the directory to add the item to</param>
+        /// <param name="item">The <see cref="VirtualItem{T}"/> to add</param>
+        /// <param name="overwrite">Indicates whether to overwrite an existing node</param>
         public void AddItem(VirtualPath itemDirectoryPath, VirtualItem<T> item, bool overwrite = false)
         {
-            // 絶対パスに変換
+            // Convert to absolute path
             itemDirectoryPath = ConvertToAbsolutePath(itemDirectoryPath).NormalizePath();
 
-            // アイテム追加対象ディレクトリを取得
+            // Retrieve the directory where the item will be added
             VirtualDirectory directory = GetDirectory(itemDirectoryPath, true);
 
-            // 既存のアイテムの存在チェック
+            // Check if the item already exists
             if (directory.NodeExists(item.Name))
             {
                 if (!overwrite)
@@ -172,48 +226,60 @@ namespace AkiraNetwork.VirtualStorageLibrary
                 }
                 else
                 {
-                    // 上書き対象がアイテムでない場合は例外をスロー
+                    // Throw an exception if the target is not an item
                     if (!ItemExists(itemDirectoryPath + item.Name))
                     {
                         throw new InvalidOperationException(string.Format(Resources.NodeIsNotVirtualItem, item.Name, typeof(T).Name));
                     }
-                    // 既存アイテムの削除
+                    // Remove the existing item
                     directory.Remove(item);
                 }
             }
 
-            // 新しいアイテムを追加
+            // Add the new item
             directory.Add(item, overwrite);
 
-            // 全てのターゲットノードタイプを更新
+            // Update all target node types in the dictionary
             UpdateAllTargetNodeTypesInDictionary();
         }
 
+        /// <summary>
+        /// Adds an item.
+        /// </summary>
+        /// <param name="itemPath">The path of the item to add</param>
+        /// <param name="data">The data for the item</param>
+        /// <param name="overwrite">Indicates whether to overwrite an existing node</param>
         public void AddItem(VirtualPath itemPath, T? data = default, bool overwrite = false)
         {
-            // 絶対パスに変換
+            // Convert to absolute path
             itemPath = ConvertToAbsolutePath(itemPath).NormalizePath();
 
-            // ディレクトリパスとアイテム名を分離
+            // Separate the directory path and item name
             VirtualPath directoryPath = itemPath.DirectoryPath;
             VirtualNodeName itemName = itemPath.NodeName;
 
-            // アイテムを作成
+            // Create the item
             VirtualItem<T> item = new(itemName, data);
 
-            // AddItemメソッドを呼び出し
+            // Call the AddItem method
             AddItem(directoryPath, item, overwrite);
         }
 
+        /// <summary>
+        /// Adds a symbolic link.
+        /// </summary>
+        /// <param name="linkDirectoryPath">The path of the directory to add the symbolic link to</param>
+        /// <param name="link">The <see cref="VirtualSymbolicLink"/> to add</param>
+        /// <param name="overwrite">Indicates whether to overwrite an existing node</param>
         public void AddSymbolicLink(VirtualPath linkDirectoryPath, VirtualSymbolicLink link, bool overwrite = false)
         {
-            // linkDirectoryPathを絶対パスに変換し正規化も行う
+            // Convert to absolute path and normalize linkDirectoryPath
             linkDirectoryPath = ConvertToAbsolutePath(linkDirectoryPath).NormalizePath();
 
-            // シンボリックリンク追加対象ディレクトリを取得
+            // Retrieve the directory where the symbolic link will be added
             VirtualDirectory directory = GetDirectory(linkDirectoryPath, true);
 
-            // 既存のノードの存在チェック
+            // Check if the node already exists
             if (directory.NodeExists(link.Name))
             {
                 if (!overwrite)
@@ -222,82 +288,94 @@ namespace AkiraNetwork.VirtualStorageLibrary
                 }
                 else
                 {
-                    // 既存のノードがシンボリックリンクでない場合は例外をスロー
+                    // Throw an exception if the existing node is not a symbolic link
                     if (!SymbolicLinkExists(linkDirectoryPath + link.Name))
                     {
                         throw new InvalidOperationException(string.Format(Resources.NodeIsNotVirtualSymbolicLink, link.Name));
                     }
-                    // 既存のシンボリックリンクを削除
+                    // Remove the existing symbolic link
                     directory.Remove(link);
                 }
             }
 
-            // 新しいシンボリックリンクを追加
+            // Add the new symbolic link
             link = (VirtualSymbolicLink)directory.Add(link);
 
             if (link.TargetPath != null)
             {
-                // シンボリックリンクを作成したディレクトリパスを基準とする
+                // Use the directory path where the symbolic link was created as the base
                 VirtualPath absoluteTargetPath = ConvertToAbsolutePath(link.TargetPath!, linkDirectoryPath).NormalizePath();
 
-                // リンク辞書にリンク情報を追加
+                // Add the link information to the dictionary
                 AddLinkToDictionary(absoluteTargetPath, linkDirectoryPath + link.Name);
             }
 
-            // 全てのターゲットノードタイプを更新
+            // Update all target node types in the dictionary
             UpdateAllTargetNodeTypesInDictionary();
         }
 
+        /// <summary>
+        /// Adds a symbolic link.
+        /// </summary>
+        /// <param name="linkPath">The path of the symbolic link</param>
+        /// <param name="targetPath">The path of the target</param>
+        /// <param name="overwrite">Indicates whether to overwrite an existing node</param>
         public void AddSymbolicLink(VirtualPath linkPath, VirtualPath? targetPath = null, bool overwrite = false)
         {
-            // linkPathを絶対パスに変換し正規化も行う
+            // Convert to absolute path and normalize linkPath
             linkPath = ConvertToAbsolutePath(linkPath).NormalizePath();
 
-            // ディレクトリパスとリンク名を分離
+            // Separate the directory path and link name
             VirtualPath directoryPath = linkPath.DirectoryPath;
             VirtualNodeName linkName = linkPath.NodeName;
 
-            // シンボリックリンクを作成
+            // Create the symbolic link
             VirtualSymbolicLink link = new(linkName, targetPath);
 
-            // AddSymbolicLinkメソッドを呼び出し
+            // Call the AddSymbolicLink method
             AddSymbolicLink(directoryPath, link, overwrite);
         }
 
+        /// <summary>
+        /// Changes the name of a node.
+        /// </summary>
+        /// <param name="nodePath">The path of the node to rename</param>
+        /// <param name="newName">The new name</param>
+        /// <param name="resolveLinks">Indicates whether to resolve symbolic links</param>
         public void SetNodeName(VirtualPath nodePath, VirtualNodeName newName, bool resolveLinks = true)
         {
             VirtualPath oldAbsolutePath = ConvertToAbsolutePath(nodePath);
             VirtualPath newAbsolutePath = oldAbsolutePath.DirectoryPath + newName;
 
-            // 新しい名前が現在の名前と同じかどうかのチェック
+            // Check if the new name is the same as the current name
             if (oldAbsolutePath == newAbsolutePath)
             {
                 throw new InvalidOperationException(string.Format(Resources.NewNameSameAsCurrent, newAbsolutePath));
             }
 
-            // ノードの取得（リンクを解決しながら）
+            // Retrieve the node (resolving links as needed)
             VirtualNodeContext nodeContext = WalkPathToTarget(oldAbsolutePath, null, null, resolveLinks, true);
             VirtualNode node = nodeContext.Node!;
 
-            // 新しい名前のノードが既に存在するかどうかのチェック
+            // Check if a node with the new name already exists
             if (NodeExists(newAbsolutePath))
             {
                 throw new InvalidOperationException(string.Format(Resources.NewNameNodeAlreadyExists, newAbsolutePath));
             }
 
-            // 親ディレクトリの取得
+            // Retrieve the parent directory
             VirtualDirectory parentDirectory = nodeContext.ParentDirectory!;
 
-            // リンク辞書の更新（シンボリックリンクの場合）
+            // Update the link dictionary (if the node is a symbolic link)
             if (node is VirtualSymbolicLink)
             {
                 UpdateLinkNameInDictionary(oldAbsolutePath, newAbsolutePath);
             }
 
-            // リンク辞書の更新（ターゲットパスの変更）
+            // Update the link dictionary (change the target path)
             UpdateLinksToTarget(oldAbsolutePath, newAbsolutePath);
 
-            // 親ディレクトリから古いノードを削除し、新しい名前のノードを追加
+            // Remove the old node from the parent directory and add the new named node
             parentDirectory.SetNodeName(node.Name, newName);
         }
     }
