44036e7f1aee87a9454688638a8232d3e1734319
下舘朗 <shimodateakira@i.softbank.jp>
2024-08-08 17:12:51 +0900

    ドキュメントビルド用バッチファイルの作成。

---
+++ build-docs.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+REM プロジェクトのビルドとXMLドキュメントの生成
+cd AkiraNetwork\VirtualStorageLibrary
+dotnet build VirtualStorageLibrary.csproj /p:DocumentationFile=bin\Debug\net8.0\VirtualStorageLibrary.xml
+
+REM DocFXによるドキュメント生成
+cd ..\..\documents
+docfx docfx.json
+
+cd ..
+
+REM 終了メッセージ
+echo ドキュメント生成が完了しました。
+endlocal
+pause
